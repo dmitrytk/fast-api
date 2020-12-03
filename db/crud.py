1a6b4d65a4197667361c44aebbe1c9cf2793ac78
Dmitry T <takkand.d@gmail.com>
2020-12-03 16:49:09 +0500

    Add tests

--- db/crud.py
+++ db/crud.py
@@ -1,10 +1,22 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
 
-def get_fields(db: Session):
+def get_fields(db: Session) -> List[models.Field]:
     return db.query(models.Field).all()
+
+
+def get_field(field_id: int, db: Session) -> models.Field:
+    field = db.query(models.Field).filter(models.Field.id == field_id).first()
+    print(field)
+    return field
+
+
+def delete_field(id: int, db: Session):
+    return db.query(models.Field).filter(models.Field.id == id).delete()
 
 
 def create_field(db: Session, field: schemas.FieldCreate):
@@ -13,3 +25,18 @@ def create_field(db: Session, field: schemas.FieldCreate):
     db.commit()
     db.refresh(db_field)
     return db_field
+
+
+def bulk_create_fields(db: Session):
+    fields = []
+    for i in range(100):
+        field = models.Field(name=f'Field{i}')
+        fields.append(field)
+    db.bulk_save_objects(fields)
+    db.commit()
+
+
+def delete_all_fields(db: Session):
+    count = db.query(models.Field).delete()
+    db.commit()
+    return count
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.testclient import TestClient
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
@@ -34,6 +35,11 @@ def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "name": "World"})
 
 
+@app.get("/hello")
+def hello(request: Request):
+    return {'message': 'Hello world!'}
+
+
 @app.post("/fields/", response_model=schemas.Field)
 def create_field(field: schemas.FieldCreate, db: Session = Depends(get_db)):
     try:
@@ -42,7 +48,48 @@ def create_field(field: schemas.FieldCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Field with this name already exists")
 
 
+@app.post("/fields/create")
+def delete_all_fields(db: Session = Depends(get_db)):
+    return crud.bulk_create_fields(db)
+
+
+@app.delete("/fields/")
+def delete_all_fields(db: Session = Depends(get_db)):
+    return crud.delete_all_fields(db)
+
+
+@app.get("/fields/{field_id}", response_model=schemas.Field)
+def read_field(field_id: int, db: Session = Depends(get_db)):
+    field = crud.get_field(field_id=field_id, db=db)
+    return field
+
+
 @app.get("/fields/", response_model=List[schemas.Field])
 def read_fields(db: Session = Depends(get_db)):
     fields = crud.get_fields(db)
     return fields
+
+
+@app.delete("/fields/{field_id}")
+def delete_field(field_id: int, db: Session = Depends(get_db)):
+    return crud.delete_field(field_id, db)
+
+
+@app.get("/sandbox")
+def sandbox():
+    return {'message': 'sandbox'}
+
+
+@app.post("/sandbox", response_model=List[schemas.WellCreate])
+def sandbox(data: List[schemas.WellCreate]):
+    print(len(data))
+    return data
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
